f49b83d0797cfdc196f455f6d304e9d3d212ca39
Sandip9016 <110584845+Sandip9016@users.noreply.github.com>
2026-05-28 16:20:57 +0530

    cors added

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ const connectDB = require("./config/db");
 require("dotenv").config();
 
 connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS
 app.use(cors());
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use("/api/vendor", require("./routes/vendorRoutes"));
 
+// Health Route
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
